a98c0e36ac997419a817be5c95ba7140cddb2f46
Jader Yepez <jader_16@hotmail.com>
2019-02-06 19:26:07 -0500

    uso de rutas y como pasarles parametros dentro de ellas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+
+// recibir parametros dentro de cada ruta
+	/*Route::get('/name/{name}',function($name){
+		return "Yo Soy ".$name;
+	});*/
+
+	/*Route::get('/name/{name}/lastname/{lastname}',function($name,$lastname){
+		return "Yo Soy ".$name." ".$lastname;
+	});*/
+
+	/*Route::get('/name/{name}/lastname/{lastname?}',function($name,$lastname=null){
+		return "Yo Soy ".$name." ".$lastname;
+	});*/
+
+	Route::get('/name/{name}/lastname/{lastname?}',function($name,$lastname='Yepez'){
+		return "Yo Soy ".$name." ".$lastname;
+	}); 
+
+	
